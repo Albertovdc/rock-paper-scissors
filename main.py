cec1bbe421c69c2f4d5c9326cbf21dbe2a63945d
Albertovdc <albertovdc.gnz@gmail.com>
2023-10-09 22:36:17 -0600

    modify main

--- main.py
+++ main.py
@@ -29,18 +29,20 @@ scissors = '''
 game = [rock, paper, scissors]
 option = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 option_computer = random.randint(0,2)
-print(game[option])
-print("Computer choice:")
-print(game[option_computer])
 if option >= 3:
-    print("You lose")
-elif option == 0 and option_computer == 2:
-    print("You win")
-elif option == 2 and option_computer == 0:
-    print("You lose")
-elif option_computer > option:
-    print("You lose")
-elif option > option_computer:
-    print("You win")
-elif option_computer == option:
-    print("It's a draw")
+    print("You losE, YOU INTRODUCE A INVALID NUMBER")
+
+else:
+    print(game[option])
+    print("Computer choice:")
+    print(game[option_computer])
+    if option == 0 and option_computer == 2:
+        print("You win")
+    elif option == 2 and option_computer == 0:
+        print("You lose")
+    elif option_computer > option:
+        print("You lose")
+    elif option > option_computer:
+        print("You win")
+    elif option_computer == option:
+        print("It's a draw")
