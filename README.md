ef96e326ef49afe12f3ad4e77dbcfe1da20ef637
Albertovdc <albertovdc.gnz@gmail.com>
2023-10-09 22:32:41 -0600

    modify main and README

---
+++ README.md
@@ -0,0 +1,26 @@
+# Random module
+````python
+import random
+````
+````python
+import random
+
+random_float = random.random() * 5
+print(random_float)
+````
+# List
+Elements within a set of square brackets are separated by commas.
+````python
+fruits = ["apple", "orange", "banana"]
+fruits[0]
+fruits[-1]
+fruits[1] = "pencil"
+fruits.append("tomato")
+fruits.extend(["no fruit", "hi"])
+````
+Create list with lists
+````python
+new_list = [list1, list2]
+list_1 = [x][y]
+
+````
---
+++ main.py
@@ -0,0 +1,46 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game = [rock, paper, scissors]
+option = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+option_computer = random.randint(0,2)
+print(game[option])
+print("Computer choice:")
+print(game[option_computer])
+if option >= 3:
+    print("You lose")
+elif option == 0 and option_computer == 2:
+    print("You win")
+elif option == 2 and option_computer == 0:
+    print("You lose")
+elif option_computer > option:
+    print("You lose")
+elif option > option_computer:
+    print("You win")
+elif option_computer == option:
+    print("It's a draw")
